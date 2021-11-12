4a362f7d364e4539f97ee6f37d6b2a6865677dbe
Federico Rossi <frossi.933@gmail.com>
2021-11-12 19:21:35 +0000

    Create server and routes objects

--- src/main/scala/com/social/network/SocialNetworkRoutes.scala
+++ src/main/scala/com/social/network/SocialNetworkRoutes.scala
@@ -2,31 +2,35 @@ package com.social.network
 
 import cats.effect.Sync
 import cats.implicits._
+import com.social.network.model.User.UserId
+import com.social.network.repositories.{PostsRepository, UsersRepository}
 import org.http4s.HttpRoutes
+import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
 import org.http4s.dsl.Http4sDsl
+
 
 object SocialNetworkRoutes {
 
-  def jokeRoutes[F[_]: Sync](J: Jokes[F]): HttpRoutes[F] = {
+  def userRoutes[F[_]: Sync](usersRepo: UsersRepository[F]): HttpRoutes[F] = {
     val dsl = new Http4sDsl[F]{}
     import dsl._
     HttpRoutes.of[F] {
-      case GET -> Root / "joke" =>
+      case GET -> Root / "user" / IntVar(id) =>
         for {
-          joke <- J.get
-          resp <- Ok(joke)
+          user <- usersRepo.getUserById(UserId(id))
+          resp <- Ok(user.get) // FIXME
         } yield resp
     }
   }
 
-  def helloWorldRoutes[F[_]: Sync](H: HelloWorld[F]): HttpRoutes[F] = {
+  def postsRoutes[F[_]: Sync](postsRepo: PostsRepository[F]): HttpRoutes[F] = {
     val dsl = new Http4sDsl[F]{}
     import dsl._
     HttpRoutes.of[F] {
-      case GET -> Root / "hello" / name =>
+      case GET -> Root / "posts" =>
         for {
-          greeting <- H.hello(HelloWorld.Name(name))
-          resp <- Ok(greeting)
+          posts <- postsRepo.getAll()
+          resp <- Ok(posts)
         } yield resp
     }
   }
--- src/main/scala/com/social/network/SocialNetworkServer.scala
+++ src/main/scala/com/social/network/SocialNetworkServer.scala
@@ -1,38 +1,32 @@
 package com.social.network
 
-import cats.effect.{ConcurrentEffect, Timer}
+import cats.effect.{ConcurrentEffect, ContextShift, ExitCode, Timer}
 import cats.implicits._
+import com.social.network.config.DBConfig
+import com.social.network.repositories.{PostsRepositoryDB, UsersRepositoryDB}
 import fs2.Stream
-import org.http4s.client.blaze.BlazeClientBuilder
 import org.http4s.implicits._
-import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.server.Router
+import org.http4s.blaze.server.BlazeServerBuilder
 import org.http4s.server.middleware.Logger
+
 import scala.concurrent.ExecutionContext.global
 
 object SocialNetworkServer {
 
-  def stream[F[_]: ConcurrentEffect](implicit T: Timer[F]): Stream[F, Nothing] = {
-    for {
-      client <- BlazeClientBuilder[F](global).stream
-      helloWorldAlg = HelloWorld.impl[F]
-      jokeAlg = Jokes.impl[F](client)
+  def stream[F[_]: ConcurrentEffect: ContextShift](implicit T: Timer[F]): Stream[F, ExitCode] = {
 
-      // Combine Service Routes into an HttpApp.
-      // Can also be done via a Router if you
-      // want to extract a segments not checked
-      // in the underlying routes.
-      httpApp = (
-        SocialNetworkRoutes.helloWorldRoutes[F](helloWorldAlg) <+>
-        SocialNetworkRoutes.jokeRoutes[F](jokeAlg)
-      ).orNotFound
+      val services = SocialNetworkRoutes.postsRoutes[F](PostsRepositoryDB(DBConfig())) <+>
+        SocialNetworkRoutes.userRoutes[F](UsersRepositoryDB(DBConfig()))
+
+      val httpApp = Router("/" -> services).orNotFound
 
       // With Middlewares in place
-      finalHttpApp = Logger.httpApp(true, true)(httpApp)
+      val finalHttpApp = Logger.httpApp(true, true)(httpApp)
 
-      exitCode <- BlazeServerBuilder[F](global)
+      BlazeServerBuilder[F](global)
         .bindHttp(8080, "0.0.0.0")
         .withHttpApp(finalHttpApp)
         .serve
-    } yield exitCode
-  }.drain
+  }
 }
