ccc49765c48848f182bbb2697c92657154ad132e
Federico Rossi <frossi.933@gmail.com>
2021-11-16 10:23:12 +0000

    Change schema and data of database

--- src/main/resources/db/migration/V2__data.sql
+++ src/main/resources/db/migration/V2__data.sql
@@ -1,18 +1,18 @@
-INSERT INTO users(id, email, name, active) VALUES
-    (1, 'diego.maradona@example.com','Diego Maradona', true),
-    (2, 'lionel.messi@example.com', 'Lionel Messi', true),
-    (3, 'marcelo.gallardo@example.com', 'Marcelo Gallardo', true);
+INSERT INTO users(id, email, name, password, active) VALUES
+    (1, 'diego.maradona@example.com','Diego Maradona', 'argentina123',true),
+    (2, 'lionel.messi@example.com', 'Lionel Messi', 'copaamerica2021', true),
+    (3, 'marcelo.gallardo@example.com', 'Marcelo Gallardo', 'riverplate123', true);
 
 ALTER SEQUENCE users_id_seq RESTART WITH 4;
 
-INSERT INTO posts(id, authorId, text, image, timestamp) VALUES
-    (1, 3, 'Well played, another victory! thank you players of River Plate for your effort!', null, '2021-01-01 01:00:00');
+INSERT INTO posts(id, authorId, text, image) VALUES
+    (1, 3, 'Well played, another victory! thank you players of River Plate for your effort!', decode('013d7d16d7ad4fefb61bd95b765c8ceb', 'hex'));
 
 ALTER SEQUENCE posts_id_seq RESTART WITH 2;
 
-INSERT INTO comments(id, postId, authorId, text, timestamp) VALUES
-    (1, 1, 1, 'You are the best!', '2021-01-01 08:00:00'),
-    (2, 1, 2, 'Amazing!!', '2021-01-02 01:00:00'),
-    (3, 1, 3, 'Thanks :)', '2021-01-03 07:00:00');
+INSERT INTO comments(id, postId, authorId, text) VALUES
+    (1, 1, 1, 'You are the best!'),
+    (2, 1, 2, 'Amazing!!'),
+    (3, 1, 3, 'Thanks :)');
 
 ALTER SEQUENCE comments_id_seq RESTART WITH 4;
