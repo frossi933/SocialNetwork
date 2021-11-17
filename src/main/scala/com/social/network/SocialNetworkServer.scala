6d9f89859b93d4eb348e9025883510ab390a3e38
Federico Rossi <frossi.933@gmail.com>
2021-11-17 00:53:35 +0000

    Update setup of server

--- src/main/scala/com/social/network/SocialNetworkServer.scala
+++ src/main/scala/com/social/network/SocialNetworkServer.scala
@@ -3,10 +3,13 @@ package com.social.network
 import cats.effect.{ConcurrentEffect, ContextShift, ExitCode, Timer}
 import cats.implicits._
 import com.social.network.config.DBConfig
-import com.social.network.repositories.{PostsRepositoryDB, UsersRepositoryDB}
+import com.social.network.model.User
+import com.social.network.repositories.{CommentsRepositoryDB, PostsRepositoryCompressionWrapper, PostsRepositoryDB, UsersRepositoryDB}
+import com.social.network.routes.{AuthRoutes, PostsRoutes, ProfileRoutes}
+import com.social.network.services.{AuthenticationServiceCookies, PostsServiceImpl, ProfileServiceImpl}
 import fs2.Stream
 import org.http4s.implicits._
-import org.http4s.server.Router
+import org.http4s.server.{AuthMiddleware, Router}
 import org.http4s.blaze.server.BlazeServerBuilder
 import org.http4s.server.middleware.Logger
 
@@ -16,17 +19,29 @@ object SocialNetworkServer {
 
   def stream[F[_]: ConcurrentEffect: ContextShift](implicit T: Timer[F]): Stream[F, ExitCode] = {
 
-      val services = SocialNetworkRoutes.postsRoutes[F](PostsRepositoryDB(DBConfig())) <+>
-        SocialNetworkRoutes.userRoutes[F](UsersRepositoryDB(DBConfig()))
+    val dbConfig = DBConfig()
+    val usersRepo = UsersRepositoryDB(dbConfig)
+    val postsRepo = PostsRepositoryCompressionWrapper(PostsRepositoryDB(dbConfig))
+    val commentsRepo = CommentsRepositoryDB(dbConfig)
+    val authService = AuthenticationServiceCookies[F](usersRepo)
+    val profileService = ProfileServiceImpl(postsRepo, commentsRepo)
+    val postsService = PostsServiceImpl(postsRepo, commentsRepo)
 
-      val httpApp = Router("/" -> services).orNotFound
+    val middleware: AuthMiddleware[F, User] =
+      AuthMiddleware(authService.authenticator, AuthRoutes.onFailure)
 
-      // With Middlewares in place
-      val finalHttpApp = Logger.httpApp(true, true)(httpApp)
+    val services =
+      AuthRoutes.routes[F](authService) <+>
+      middleware(PostsRoutes.authedRoutes[F](postsService)) <+>
+      middleware(ProfileRoutes.authedRoutes[F](profileService, postsService))
 
-      BlazeServerBuilder[F](global)
-        .bindHttp(8080, "0.0.0.0")
-        .withHttpApp(finalHttpApp)
-        .serve
+    val httpApp = Router("/" -> services).orNotFound
+
+    val finalHttpApp = Logger.httpApp(true, true)(httpApp)
+
+    BlazeServerBuilder[F](global)
+      .bindHttp(8080, "0.0.0.0")
+      .withHttpApp(finalHttpApp)
+      .serve
   }
 }
